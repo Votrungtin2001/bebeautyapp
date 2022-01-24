9bdd1bf949b5f589a5d8c6bf4ea291c175f45bb9
minhthi-uit <19522257@gm.uit.edu.vn>
2022-01-24 14:00:02 +0700

    first update

---
+++ lib/contants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+
+
+const kPrimaryColor = Color(0xFFFFA0A0);
+const kSecondaryColor = Color(0xFF262A53);
+const kThirdColor = Color(0xFF628395);
+const kFourthColor = Color(0xFFFFE3E3);
+const kTextColor = Color(0xFF333333);
+
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
+);
+
+// Form Error
+final RegExp emailValidatorRegExp = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
+const String kEmailNullError = "Please Enter your email";
+const String kInvalidEmailError = "Please Enter Valid Email";
+const String kPassNullError = "Please Enter your password";
+const String kShortPassError = "Password is too short";
+const String kMatchPassError = "Passwords don't match";
+const String kNamelNullError = "Please Enter your name";
+const String kPhoneNumberNullError = "Please Enter your phone number";
+const String kAddressNullError = "Please Enter your address";
+
