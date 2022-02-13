08caacc89b719c3a61e3b4533a44de89c2ae8878
Bùi Phú <19522018@gm.uit.edu.vn>
2022-02-13 13:42:22 +0700

    fix ban phim

--- lib/ui/authenication/login/forgot_password_screen.dart
+++ lib/ui/authenication/login/forgot_password_screen.dart
@@ -1,27 +1,32 @@
-import 'package:bebeautyapp/ui/authenication/login/widgets/rounded_input_field.dart';
 import 'package:bebeautyapp/ui/authenication/register/widgets/custom_rounded_loading_button.dart';
 import 'package:cool_alert/cool_alert.dart';
-import 'package:bebeautyapp/model/login/login_view_model.dart';
 import 'package:bebeautyapp/contants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:rounded_loading_button/rounded_loading_button.dart';
 
 
-class ForgotPasswordScreen extends StatelessWidget {
+class ForgotPasswordScreen extends StatefulWidget {
   static String id = "ForgotPasswordScreen";
+
+
+  @override
+  _ForgotPasswordScreen createState() => new _ForgotPasswordScreen();
+}
+class _ForgotPasswordScreen extends State<ForgotPasswordScreen> {
+  final _formKey = GlobalKey<FormState>();
   final sendEmailButtonController = RoundedLoadingButtonController();
   final emailFocusNode = FocusNode();
   final emailController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
+
   String email = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffFFE3E3),
-      body: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
@@ -46,7 +51,7 @@ class ForgotPasswordScreen extends StatelessWidget {
                   style: kPop400TextStyle,
                   textAlign: TextAlign.center,
                 ),
-                ),
+              ),
 
               SizedBox(
                 height: 26,
